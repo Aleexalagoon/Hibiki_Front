dbbfcb00d5b7c3c269c60776356d76ff98e5c795
Reda <a27338@svalero.com>
2025-02-10 19:12:15 +0100

    Carrito/Home

--- Hibiki/src/views/NovedadesView.vue
+++ Hibiki/src/views/NovedadesView.vue
@@ -1,47 +1,221 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
 const sliderContent = [
   {
     titulo: "Hurry Up Tomorrow",
     subtitulo: "The Weeknd",
-    imagen: "https://www.sentireascoltare.com/wp-content/uploads/2025/02/weeknd-hurry-up-tomorrow.jpg",
+    imagen:
+      "https://www.sentireascoltare.com/wp-content/uploads/2025/02/weeknd-hurry-up-tomorrow.jpg",
   },
   {
     titulo: "Benidorm Fest 2025",
     subtitulo: "Apple Music",
-    imagen: "https://is1-ssl.mzstatic.com/image/thumb/Features126/v4/17/85/9a/17859af8-c050-20d0-ea94-d05c7dd3551d/d4530ed8-ac03-44e9-b37c-f32e4d89fe7e.png/530x304sr.webp",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Features221/v4/21/4a/c3/214ac3df-2e73-9b6e-3603-5a92c9361ad5/source/1200x300bb.webp",
   },
   {
     titulo: "MORA AMA AL CHIQUI",
     subtitulo: "Mora",
-    imagen: "https://frasesencanciones.com/wp-content/uploads/2023/11/mora-cantante-768x513.jpg",
+    imagen:
+      "https://frasesencanciones.com/wp-content/uploads/2023/11/mora-cantante-768x513.jpg",
   },
 ];
 
+
 const cancionesRecientes = [
-  { titulo: "Cry For Me", artista: "The Weeknd", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/88x88bb.jpg" },
-  { titulo: "M.A.P.S.", artista: "Amaia", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/88x88bb.jpg" },
-  { titulo: "KAIMAN", artista: "Rigoberta Bandini", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/79/10/e8/7910e8d8-caa9-9d20-9e30-f8e363af03e7/199066494011.jpg/88x88bb.jpg" },
-  { titulo: "FABERGÉ", artista: "Recycled J · HOKE · Ébano", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/ab/54/6a/ab546a81-a18c-2d3d-087f-b2e8d7630554/5021732620965.jpg/88x88bb.jpg" },
-  { titulo: "Cry For Me", artista: "The Weeknd", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/88x88bb.jpg" },
-  { titulo: "M.A.P.S.", artista: "Amaia", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/88x88bb.jpg" },
-  { titulo: "KAIMAN", artista: "Rigoberta Bandini", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/79/10/e8/7910e8d8-caa9-9d20-9e30-f8e363af03e7/199066494011.jpg/88x88bb.jpg" },
-  { titulo: "FABERGÉ", artista: "Recycled J · HOKE · Ébano", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/ab/54/6a/ab546a81-a18c-2d3d-087f-b2e8d7630554/5021732620965.jpg/88x88bb.jpg" },
+  {
+    titulo: "Cry For Me",
+    artista: "The Weeknd",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/88x88bb.jpg",
+  },
+  {
+    titulo: "M.A.P.S.",
+    artista: "Amaia",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/88x88bb.jpg",
+  },
+  {
+    titulo: "KAIMAN",
+    artista: "Rigoberta Bandini",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/79/10/e8/7910e8d8-caa9-9d20-9e30-f8e363af03e7/199066494011.jpg/88x88bb.jpg",
+  },
+  {
+    titulo: "FABERGÉ",
+    artista: "Recycled J · HOKE · Ébano",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/ab/54/6a/ab546a81-a18c-2d3d-087f-b2e8d7630554/5021732620965.jpg/88x88bb.jpg",
+  },
+  {
+    titulo: "Cry For Me",
+    artista: "The Weeknd",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/88x88bb.jpg",
+  },
+  {
+    titulo: "M.A.P.S.",
+    artista: "Amaia",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/88x88bb.jpg",
+  },
+  {
+    titulo: "KAIMAN",
+    artista: "Rigoberta Bandini",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/79/10/e8/7910e8d8-caa9-9d20-9e30-f8e363af03e7/199066494011.jpg/88x88bb.jpg",
+  },
+  {
+    titulo: "FABERGÉ",
+    artista: "Recycled J · HOKE · Ébano",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/ab/54/6a/ab546a81-a18c-2d3d-087f-b2e8d7630554/5021732620965.jpg/88x88bb.jpg",
+  },
 ];
 
 const nuevosLanzamientos = [
-  { titulo: "Hurry Up Tomorrow", artista: "The Weeknd", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/316x316bb.webp" },
-  { titulo: "Si Abro Los Ojos", artista: "Amaia", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/316x316bb.webp" },
-  { titulo: "Al romper la burbuja", artista: "Joaquina", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp" },
-  { titulo: "Al romper la burbuja", artista: "Joaquina", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp" },
-  { titulo: "Hurry Up Tomorrow", artista: "The Weeknd", imagen: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/316x316bb.webp" },
+  {
+    titulo: "Hurry Up Tomorrow",
+    artista: "The Weeknd",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Si Abro Los Ojos",
+    artista: "Amaia",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
+  {
+    titulo: "Hurry Up Tomorrow",
+    artista: "The Weeknd",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Si Abro Los Ojos",
+    artista: "Amaia",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
+  {
+    titulo: "Hurry Up Tomorrow",
+    artista: "The Weeknd",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Si Abro Los Ojos",
+    artista: "Amaia",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
+  {
+    titulo: "Hurry Up Tomorrow",
+    artista: "The Weeknd",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Si Abro Los Ojos",
+    artista: "Amaia",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/c5/d3/82/c5d38265-9bd7-88aa-3bb3-9500eceed9d6/24UM1IM01951.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
+  {
+    titulo: "Hurry Up Tomorrow",
+    artista: "The Weeknd",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/3d/0d/06/3d0d06e6-cfd4-8f69-4a42-52b7cf33fb24/25UMGIM09490.rgb.jpg/316x316bb.webp",
+  },
+  {
+    titulo: "Al romper la burbuja",
+    artista: "Joaquina",
+    imagen:
+      "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/3b/19/cf/3b19cf3e-b9a5-1c55-47ec-74f6bbfe92b2/Jobaddabaee-3bcc-4d7d-a531-e6ff80ec4b06-184757849-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_361394388_2036964865-Time1738151215966.png/316x316bb.webp",
+  },
 ];
 
-const iniciarSesion = () => {
-  window.location.href = '/login'; 
+
+const currentSlide = ref(0);
+const totalSlides = sliderContent.length;
+let slideInterval;
+
+const nextSlide = () => {
+  currentSlide.value = (currentSlide.value + 1) % totalSlides;
 };
-const registrar= () => {
-  window.location.href = '/register'; 
+
+const prevSlide = () => {
+  currentSlide.value = (currentSlide.value - 1 + totalSlides) % totalSlides;
 };
+
+const startSlideInterval = () => {
+  slideInterval = setInterval(() => {
+    nextSlide();
+  }, 7000); 
+};
+
+const stopSlideInterval = () => {
+  clearInterval(slideInterval);
+};
+
+onMounted(() => {
+  startSlideInterval();
+});
+
+onUnmounted(() => {
+  stopSlideInterval();
+});
+
 </script>
 
 <template>
@@ -50,20 +224,36 @@ const registrar= () => {
       <button class="register-button" @click="registrar">Registrarse</button>
       <button class="login-button" @click="iniciarSesion">Iniciar Sesión</button>
     </div>
+
     <section class="slider">
-      <div v-for="(slide, index) in sliderContent" :key="index" class="slide">
-        <img :src="slide.imagen" :alt="slide.titulo" />
-        <div class="slide-content">
-          <h2>{{ slide.titulo }}</h2>
-          <p>{{ slide.subtitulo }}</p>
+      <div class="carousel-container">
+        <div class="carousel">
+          <div
+            v-for="(slide, index) in sliderContent"
+            :key="index"
+            class="slide"
+            :class="{ active: index === currentSlide }"
+          >
+            <img :src="slide.imagen" :alt="slide.titulo" />
+            <div class="slide-content">
+              <h2>{{ slide.titulo }}</h2>
+              <p>{{ slide.subtitulo }}</p>
+            </div>
+          </div>
         </div>
+        <button class="prev" @click="() => { prevSlide(); stopSlideInterval(); startSlideInterval(); }">‹</button>
+        <button class="next" @click="() => { nextSlide(); stopSlideInterval(); startSlideInterval(); }">›</button>
       </div>
     </section>
 
     <section class="canciones-recientes">
       <h2>Canciones recién llegadas</h2>
       <div class="canciones-grid">
-        <div v-for="(cancion, index) in cancionesRecientes" :key="index" class="cancion-item">
+        <div
+          v-for="(cancion, index) in cancionesRecientes"
+          :key="index"
+          class="cancion-item"
+        >
           <img :src="cancion.imagen" :alt="cancion.titulo" />
           <div>
             <h3>{{ cancion.titulo }}</h3>
@@ -74,16 +264,19 @@ const registrar= () => {
     </section>
 
     <section class="nuevos-lanzamientos">
-  <h2 class="section-title">Nuevos lanzamientos</h2>
-  <div class="album-grid">
-    <div v-for="(album, index) in nuevosLanzamientos" :key="index" class="album-item">
-      <img :src="album.imagen" :alt="album.titulo" class="album-img" />
-      <h3 class="album-title">{{ album.titulo }}</h3>
-      <p class="album-artist">{{ album.artista }}</p>
-    </div>
-  </div>
-</section>
-
+      <h2 class="section-title">Nuevos lanzamientos</h2>
+      <div class="album-grid">
+        <div
+          v-for="(album, index) in nuevosLanzamientos"
+          :key="index"
+          class="album-item"
+        >
+          <img :src="album.imagen" :alt="album.titulo" class="album-img" />
+          <h3 class="album-title">{{ album.titulo }}</h3>
+          <p class="album-artist">{{ album.artista }}</p>
+        </div>
+      </div>
+    </section>
   </div>
 
   <footer class="footer">
@@ -98,7 +291,6 @@ const registrar= () => {
     </div>
   </footer>
 </template>
-
 
 <style scoped>
 .novedades {
@@ -147,29 +339,44 @@ const registrar= () => {
   margin-top: -37px;
 }
 
-.login-button:hover {
-  background-color: #ca3900;
+.carousel-container {
+  width: 100%;
+  max-width: 1200px; 
+  margin: 0 auto;
+  position: relative;
+  overflow: hidden;
+  border-radius: 10px;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15);
 }
 
-.slider {
+.carousel {
   display: flex;
-  gap: 17rem;
-  overflow-x: auto;
-  padding: 1rem;
-  background: #000;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
+  position: relative;
+  height: 500px; 
 }
 
 .slide {
-  flex: 0 0 auto;
+  min-width: 100%;
+  opacity: 0;
+  transform: translateX(100%);
+  transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.slide.active {
+  opacity: 1;
+  transform: translateX(0);
   position: relative;
-  width: 300px;
 }
 
 .slide img {
   width: 100%;
-  border-radius: 8px;
+  height: 100%;
+  object-fit: cover;
+  display: block;
+  border-radius: 10px;
 }
 
 .slide-content {
@@ -178,10 +385,44 @@ const registrar= () => {
   left: 0;
   right: 0;
   background: rgba(0, 0, 0, 0.6);
-  padding: 1rem;
   color: white;
-  border-radius: 0 0 8px 8px;
+  padding: 1.5rem; 
+  text-align: center;
+  border-radius: 0 0 10px 10px;
 }
+
+.prev,
+.next {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  border: none;
+  border-radius: 50%;
+  width: 50px; 
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  z-index: 10;
+  transition: background 0.3s;
+}
+
+.prev:hover,
+.next:hover {
+  background: rgba(0, 0, 0, 0.7);
+}
+
+.prev {
+  left: 20px;
+}
+
+.next {
+  right: 20px;
+}
+
 
 .album-grid {
   display: grid;
@@ -219,17 +460,6 @@ const registrar= () => {
   margin-bottom: 0.5rem;
 }
 
-.album-content h3 {
-  font-size: 1rem;
-  margin-bottom: 0.5rem;
-  color: #333;
-}
-
-.album-content p {
-  font-size: 0.875rem;
-  color: #777;
-}
-
 .album-title {
   font-size: 1rem;
   margin: 0.5rem 0 0.25rem;
@@ -243,55 +473,36 @@ const registrar= () => {
 }
 
 .canciones-grid {
-  display: grid
-;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 1rem;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
 }
 
 .cancion-item {
-    display: flex
-;
-    align-items: center;
-    gap: 0.5rem;
-    background: white;
-    border-radius: 8px;
-    padding: 0.5rem;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  background: white;
+  border-radius: 8px;
+  padding: 0.5rem;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
 }
 
 .cancion-item img {
-  width: 50px; 
-  height: 50px; 
-  border-radius: 5px; 
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
 }
 
 .cancion-item h3 {
-  font-size: 0.75rem; 
-  margin-bottom: 0.1rem; 
+  font-size: 0.75rem;
+  margin-bottom: 0.1rem;
 }
 
 .cancion-item p {
-  font-size: 0.65rem; 
-  margin: 0; 
-}
-
-.cancion-item::before,
-.cancion-item::after {
-  content: "";
-  position: absolute;
-  left: 0;
-  width: 100%;
-  height: 1px; 
-  background-color:rgb(62, 62, 62); 
-}
-
-.cancion-item::before {
-  top: 0;
-}
-
-.cancion-item::after {
-  bottom: 0;
+  font-size: 0.65rem;
+  margin: 0;
 }
 
 .footer {
@@ -326,7 +537,7 @@ const registrar= () => {
 }
 
 .footer-link:hover {
-  color: #ff5100; 
+  color: #ff5100;
 }
 
 .nuevos-lanzamientos {
